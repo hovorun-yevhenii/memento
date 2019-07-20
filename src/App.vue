35f9e8e83f1ad78c8bad8946da37c8c7f442c9e1
oxygene4 <5797565@gmail.com>
2019-07-20 12:58:45 +0300

    install basic dependencies

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
