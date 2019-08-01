1792318cbfbd490e5cc2fe09f403be8976693383
Yevhenii Hovorun <yevhenii.hovorun@rhinocodes.com>
2019-08-01 13:29:10 +0300

    add AppHeader spec

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,11 +1,10 @@
-import {shallowMount, createLocalVue} from '@vue/test-utils'
+import Vue from 'vue'
 import Vuex from 'vuex'
+import {mount} from '@vue/test-utils'
+import {state, getters} from '@/store/mutations.js'
 import AppHeader from '@/components/AppHeader.vue'
-import {getters} from '@/store/mutations.js'
 
-const localVue = createLocalVue();
-
-localVue.use(Vuex);
+Vue.use(Vuex);
 
 describe('AppHeader', () => {
     const mutations = {
@@ -13,25 +12,19 @@ describe('AppHeader', () => {
     };
 
     const store = new Vuex.Store({
+        state,
         getters,
-        mutations,
-        state: {
-            notes: [],
-            noteToUpdate: null,
-            isModalFormOpen: false,
-            searchText: '',
-        }
+        mutations
     });
 
-    const wrapper = shallowMount(AppHeader, {store, localVue});
+    const wrapper = mount(AppHeader, {store});
 
     it('renders add button', () => {
         expect(wrapper.contains('.header__add')).toBe(true);
     });
 
-    // it('calls "openForm" mutation', () => {
-    //     wrapper.find('.header__add').trigger('click');
-    //
-    //     expect(mutations.openForm).toHaveBeenCalled();
-    // })
+    it('commits an openForm mutation', () => {
+        wrapper.find('.header__add').trigger('keydown');
+        expect(mutations.openForm).toHaveBeenCalled();
+    });
 });
