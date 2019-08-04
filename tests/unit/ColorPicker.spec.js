5c8c8701751f2710496aebf727bbda180861ae12
oxygene4 <5797565@gmail.com>
2019-08-04 19:07:13 +0300

    add ModalForm spec

--- tests/unit/ColorPicker.spec.js
+++ tests/unit/ColorPicker.spec.js
@@ -2,13 +2,15 @@ import { shallowMount } from '@vue/test-utils';
 import ColorInput from '@/components/ColorInput.vue';
 
 describe('ColorInput', () => {
-  it('', () => {
-    const wrapper = shallowMount(ColorInput);
+  const wrapper = shallowMount(ColorInput);
 
+  it('Shows picker on mouseenter', () => {
     wrapper.trigger('mouseenter');
     expect(wrapper.vm.$data.showColorPicker).toBe(true);
     expect(wrapper.contains('.picker__color')).toBeTruthy();
+  });
 
+  it('Emits change event', () => {
     wrapper.find('.picker__color').trigger('click');
     expect(wrapper.emitted().change).toBeTruthy();
   });
--- tests/unit/ModalForm.spec.js
+++ tests/unit/ModalForm.spec.js
@@ -1,8 +1,28 @@
-import { shallowMount } from '@vue/test-utils';
-import ImageInput from '@/components/ImageInput.vue';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import {mount} from '@vue/test-utils';
+import {getters} from '@/store/mutations';
+import ModalForm from '@/components/ModalForm.vue';
 
-describe('ImageInput', () => {
-  it('', () => {
+Vue.use(Vuex);
 
+describe('ModalForm', () => {
+  const mutations = {
+    closeForm: jest.fn(),
+  };
+
+  const store = new Vuex.Store({
+    state: {
+      isModalFormOpen: true
+    },
+    getters,
+    mutations
+  });
+
+  const wrapper = mount(ModalForm, {store});
+
+  it('Emits closeForm event', () => {
+    wrapper.find('.dialog').trigger('mousedown');
+    expect(mutations.closeForm).toHaveBeenCalled();
   })
 });
